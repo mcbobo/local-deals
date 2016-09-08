caa119293df0412f673397aec96e8e701625e2be
Bo Li <bosupreme@gmail.com>
2016-09-08 14:45:07 -0400

    Working fileserver, and using productPanel component/directive now

--- app/components/productPanel.js
+++ app/components/productPanel.js
@@ -1,10 +1,5 @@
 app.directive("productPanel", function($log) {
   return {
-    // link: function(scope, element, attrs) {
-    //   $log.log(scope, element, attrs);
-    //   scope.$watch("products", function(products) {
-    //   });
-    // },
     restrict: "E",
     scope: {
       products: '='
